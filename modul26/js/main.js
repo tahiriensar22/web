4fe0bc9606607015a776edc07a803f3538e5b77f
tahiriensar22 <bardhmaliqi2@gmail.com>
2026-06-11 17:48:04 +0200

    commit 26

---
+++ modul26/js/main.js
@@ -0,0 +1,47 @@
+console.log("hello from Xhafa")
+
+var x=5;
+
+var y=23+53;
+console.log(y);
+
+var b=8%2;
+console.log(b);
+
+var c=99;
+c++;
+console.log(c);
+
+var c=99;
+c--;
+console.log(c);
+
+var q=4;
+var p=q**2;
+console.log(q);
+
+var b=6;
+b-=2;
+console.log(b);
+
+
+var b=6;
+var a=7;
+console.log(a==b);
+
+var b=7;
+var a="7";
+console.log(a===b);
+
+var  a=34;
+var b=54;
+console.log(a<b);
+
+var a=34;
+var b=54;
+console.log(a!=b);
+
+var name="Ensar"
+var lastname="Tahiri"
+var fullname=name + " " + lastname;
+console.log(fullname);
